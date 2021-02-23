0bab698db774f12f93bdfb2ea758fec671ed7bd9
enu <otoriden@gmail.com>
2021-02-23 17:36:30 +0900

    LocalizationKeyをUniRxで処理する

--- Assets/Package/Runtime/LocalizationKey.cs
+++ Assets/Package/Runtime/LocalizationKey.cs
@@ -2,6 +2,173 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#if TSKT_LOCALIZATION_SUPPORT_UNIRX
+using UniRx;
+
+namespace TSKT
+{
+    public readonly struct LocalizationKey
+    {
+        public readonly ReactiveProperty<SystemLanguage> language;
+        public readonly ReadOnlyReactiveProperty<string> property;
+
+        static public LocalizationKey CreateRaw(string rawString)
+        {
+            var language = new ReactiveProperty<SystemLanguage>(Localization.currentLanguage.Value);
+            var property = new ReactiveProperty<string>(rawString).ToReadOnlyReactiveProperty();
+            return new LocalizationKey(language, property);
+        }
+
+        LocalizationKey(ReactiveProperty<SystemLanguage> language, ReadOnlyReactiveProperty<string> property)
+        {
+            this.language = language;
+            this.property = property;
+        }
+
+        public LocalizationKey(string key)
+        {
+            language = new ReactiveProperty<SystemLanguage>(Localization.currentLanguage.Value);
+            property = language
+                .Select(_ => Localization.Get(_, key))
+                .ToReadOnlyReactiveProperty();
+        }
+
+        public LocalizationKey(string key, params (string key, string value)[] args)
+        {
+            language = new ReactiveProperty<SystemLanguage>(Localization.currentLanguage.Value);
+            property = language
+                .Select(lang =>
+                {
+                    var t = Localization.Get(lang, key);
+                    foreach (var it in args)
+                    {
+                        t = t.Replace(it.key, it.value);
+                    }
+                    return t;
+                })
+                .ToReadOnlyReactiveProperty();
+        }
+
+        public LocalizationKey(string key, params (string key, LocalizationKey value)[] args)
+        {
+            language = new ReactiveProperty<SystemLanguage>(Localization.currentLanguage.Value);
+            property = language
+                .Select(lang =>
+                {
+                    var t = Localization.Get(lang, key);
+                    foreach (var it in args)
+                    {
+                        t = t.Replace(it.key, it.value.Localize(lang));
+                    }
+                    return t;
+                })
+                .ToReadOnlyReactiveProperty();
+        }
+
+        public LocalizationKey(int index)
+        {
+            language = new ReactiveProperty<SystemLanguage>(Localization.currentLanguage.Value);
+            property = language
+                .Select(_ => Localization.Get(_, index))
+                .ToReadOnlyReactiveProperty();
+        }
+
+        public LocalizationKey(int index, params (string key, LocalizationKey value)[] args)
+        {
+            language = new ReactiveProperty<SystemLanguage>(Localization.currentLanguage.Value);
+            property = language
+                .Select(lang =>
+                {
+                    var t = Localization.Get(lang, index);
+                    foreach (var it in args)
+                    {
+                        t = t.Replace(it.key, it.value.Localize(lang));
+                    }
+                    return t;
+                })
+                .ToReadOnlyReactiveProperty();
+        }
+
+        public LocalizationKey(int index, params (string key, string value)[] args)
+        {
+            language = new ReactiveProperty<SystemLanguage>(Localization.currentLanguage.Value);
+            property = language
+                .Select(lang =>
+                {
+                    var t = Localization.Get(lang, index);
+                    foreach (var it in args)
+                    {
+                        t = t.Replace(it.key, it.value);
+                    }
+                    return t;
+                })
+                .ToReadOnlyReactiveProperty();
+        }
+
+
+        readonly public LocalizationKey Replace(params (string key, string value)[] args)
+        {
+            var prop = property?.Select(_ =>
+            {
+                foreach (var (key, value) in args)
+                {
+                    _ = _.Replace(key, value);
+                }
+                return _;
+            }).ToReadOnlyReactiveProperty();
+
+            return new LocalizationKey(language, prop);
+        }
+
+        readonly public LocalizationKey Replace(params (string key, LocalizationKey value)[] args)
+        {
+            var lang = language;
+            var prop = property?.Select(_ =>
+            {
+                foreach (var (key, value) in args)
+                {
+                    _ = _.Replace(key, value.Localize(lang.Value));
+                }
+                return _;
+            }).ToReadOnlyReactiveProperty();
+
+            return new LocalizationKey(language, prop);
+        }
+
+        readonly public string Localize()
+        {
+            if (language != null)
+            {
+                language.Value = Localization.CurrentLanguage;
+            }
+            return property?.Value ?? "";
+        }
+
+        readonly public string Localize(SystemLanguage language)
+        {
+            if (this.language != null)
+            {
+                this.language.Value = language;
+            }
+            return property?.Value ?? "";
+        }
+
+        readonly public bool Empty
+        {
+            get
+            {
+                if (property == null)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
+
+#else
+
 namespace TSKT
 {
     public readonly struct LocalizationKey
@@ -164,3 +331,4 @@ namespace TSKT
         }
     }
 }
+#endif
