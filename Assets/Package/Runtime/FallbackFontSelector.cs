f1999ae0052f252837dd5ea899c71b1cf2efabed
enue <otoriden@gmail.com>
2023-11-28 09:17:42 +0900

    FallbackFontSelector

---
+++ Assets/Package/Runtime/FallbackFontSelector.cs
@@ -0,0 +1,44 @@
+﻿#nullable enable
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using TMPro;
+using Cysharp.Threading.Tasks;
+
+namespace TSKT
+{
+    public class FallbackFontSelector : MonoBehaviour
+    {
+        [System.Serializable]
+        struct LanguageFontPair
+        {
+            public SystemLanguage language;
+            public TMP_FontAsset font;
+        }
+
+        [SerializeField]
+        int destinationIndex;
+
+        [SerializeField]
+        LanguageFontPair[] pairs = default!;
+
+        void Start()
+        {
+            var defaultFont = TMP_Settings.fallbackFontAssets[destinationIndex];
+            Localization.currentLanguage.SubscribeWithState(defaultFont, (language, _defaultFont) =>
+            {
+                var index = System.Array.FindIndex(pairs, _ => _.language == language);
+
+                if (index >= 0)
+                {
+                    TMP_Settings.fallbackFontAssets[destinationIndex] = pairs[index].font;
+                }
+                else
+                {
+                    TMP_Settings.fallbackFontAssets[destinationIndex] = _defaultFont;
+                }
+            }).AddTo(destroyCancellationToken);
+        }
+    }
+}
